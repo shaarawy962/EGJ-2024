b08a1e8a89496c5dbcb23a8595c43181a81b35e5
Moaaz W. Lotfi <muazwaleed12@hotmail.com>
2024-02-22 18:17:54 +0200

    restore Enemy.cs from commit 083aae0

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,23 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Stats
+    public float maxHealth;
+    public float movementSpeed;
+    public float threshold;
+    public float runAwayMultiplier;
+
+    private bool defeated = false;
+    private bool passedThreshold = false;
+    private float health;
+    private FloatingHealthBar healthBar;
+
+    [SerializeField] GameObject target;
+
+    private void Awake()
     {
-        
+        health = maxHealth;
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Debugging threshold line
+        Debug.DrawLine(new Vector2(threshold, -1000), new Vector2(threshold, 1000), Color.red, 2.5f);
+
+        if (health <= 0 && defeated == false)
+        {
+            defeated = true;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            movementSpeed *= runAwayMultiplier;
+        }
+
+        if (transform.position.x <= threshold)
+        {
+            passedThreshold = true;
+        }
+
+        Vector2 newTarget;
+        if (passedThreshold)
+        {
+            newTarget = target.transform.position;
+        }
+        else
+        {
+            // Do not modify "y" component
+            newTarget = new(target.transform.position.x, transform.position.y);
+        }
+
+        Vector2 direction = (newTarget - (Vector2)transform.position);
+        direction = direction.normalized;
+        direction = defeated ? -direction : direction;
+
+        float step = movementSpeed * Time.deltaTime;
+        transform.position += (Vector3)direction * step;
     }
 
     public void Flee()
     {
-        
+        health = 0;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject, 1.0f);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        healthBar.UpdateHealthBar(health, maxHealth);
     }
 }
